92863e3d2be47675d4946097d247c5295c800cca
Neil <tgfc054@tgfc.tw>
2019-07-15 10:44:39 +0800

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,5 @@
-// module.exports = {
-//     publicPath: process.env.NODE_ENV === 'production'
-//         ? '/f2e-2019-1/'
-//         : '/'
-// }
-
 module.exports = {
-    publicPath: '/f2e-2019-1/'
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/f2e-2019-1/'
+        : '/'
 }
